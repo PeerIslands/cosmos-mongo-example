61c7f1a1fa2dbf937f361dc5f95876aff4b48a36
rajeshvinayagam-lab <76505827+rajeshvinayagam-lab@users.noreply.github.com>
2021-03-12 18:35:55 -0500

    Sample Project

---
+++ TestMongo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace ConsAppMongoDBCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MongoClient dbClient = new MongoClient("YOUR CONNECTION STRING");
+
+                //Database List  
+                var dbList = dbClient.ListDatabases().ToList();
+
+                Console.WriteLine("The list of databases are :");
+                foreach (var item in dbList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("\n\n");
+
+                //Get Database and Collection  
+                IMongoDatabase db = dbClient.GetDatabase("MyWorkMongoDB");
+                var collList = db.ListCollections().ToList();
+                Console.WriteLine("The list of collections are :");
+                foreach (var item in collList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var personColl = db.GetCollection<BsonDocument>("PersonsCollection");
+
+                //CREATE  
+                BsonElement personFirstNameElement = new BsonElement("PersonFirstName", "Sankhojjal");
+
+                BsonDocument personDoc = new BsonDocument();
+                personDoc.Add(personFirstNameElement);
+                personDoc.Add(new BsonElement("PersonAge", 23));
+
+                personColl.InsertOne(personDoc);
+
+                //UPDATE  
+                BsonElement updatePersonFirstNameElement = new BsonElement("PersonFirstName", "Souvik");
+
+                BsonDocument updatePersonDoc = new BsonDocument();
+                updatePersonDoc.Add(updatePersonFirstNameElement);
+                updatePersonDoc.Add(new BsonElement("PersonAge", 24));
+
+                BsonDocument findPersonDoc = new BsonDocument(new BsonElement("PersonFirstName", "Sankhojjal"));
+
+                var updateDoc = personColl.FindOneAndReplace(findPersonDoc, updatePersonDoc);
+
+                Console.WriteLine(updateDoc);
+
+                //DELETE  
+                BsonDocument findAnotherPersonDoc = new BsonDocument(new BsonElement("PersonFirstName", "Sourav"));
+
+                personColl.FindOneAndDelete(findAnotherPersonDoc);
+
+                //READ  
+                var resultDoc = personColl.Find(new BsonDocument()).ToList();
+                foreach (var item in resultDoc)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
